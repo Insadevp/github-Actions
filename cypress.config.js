2a4b3f0d434295b1d8b9cc3e0ab36cfbe23fb9f0
insaf <insaf.saadaoui83@gmail.com>
2024-07-15 16:23:02 +0200

    fdifjizduf

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reportDir: "mochawesome-report/results",
+    reportDir: 'cypress/reports', // Directory where the report will be saved
     overwrite: false,
-    html: false,
+    html: true,
     json: true
   },
   e2e: {
